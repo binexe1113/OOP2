44d1a7796f4ffb18021de7811a22c165a56699ab
Fabio Henrique Lopes <fabiohblopes3@gmail.com>
2026-06-21 23:07:55 -0300

    Adiciona getters e setters

--- codigofonte/project-main/src/main/java/com/academia/model/Aluno.java
+++ codigofonte/project-main/src/main/java/com/academia/model/Aluno.java
@@ -1,4 +1,6 @@
-public class Aluno{
+package com.academia.model;
+
+public class Aluno {
     private int idAluno;
     private String nome;
     private String cpf;
@@ -12,19 +14,117 @@ public class Aluno{
     private Academia academia;
     private Usuario usuario;
 
+    public Aluno() {
+    }
 
-public Aluno(int idAluno, String nome, String cpf, int idade, String email, String telefone, Matricula matricula, Treino treino, Aula aula, AvaliacaoFisica avaliacaoFisica, Academia academia) {
-    this.idAluno = idAluno;
-    this.nome = nome;
-    this.cpf = cpf;
-    this.idade = idade;
-    this.email = email;
-    this.telefone = telefone;
-    this.matricula = matricula;
-    this.treino = treino;
-    this.aula = aula;
-    this.avaliacaoFisica = avaliacaoFisica;
-    this.academia = academia;
-}
+    public Aluno(int idAluno, String nome, String cpf, int idade, String email, String telefone, Matricula matricula, Treino treino, Aula aula, AvaliacaoFisica avaliacaoFisica, Academia academia, Usuario usuario) {
+        this.idAluno = idAluno;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.email = email;
+        this.telefone = telefone;
+        this.matricula = matricula;
+        this.treino = treino;
+        this.aula = aula;
+        this.avaliacaoFisica = avaliacaoFisica;
+        this.academia = academia;
+        this.usuario = usuario;
+    }
 
+    public int getIdAluno() {
+        return idAluno;
+    }
+
+    public void setIdAluno(int idAluno) {
+        this.idAluno = idAluno;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Matricula getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Matricula matricula) {
+        this.matricula = matricula;
+    }
+
+    public Treino getTreino() {
+        return treino;
+    }
+
+    public void setTreino(Treino treino) {
+        this.treino = treino;
+    }
+
+    public Aula getAula() {
+        return aula;
+    }
+
+    public void setAula(Aula aula) {
+        this.aula = aula;
+    }
+
+    public AvaliacaoFisica getAvaliacaoFisica() {
+        return avaliacaoFisica;
+    }
+
+    public void setAvaliacaoFisica(AvaliacaoFisica avaliacaoFisica) {
+        this.avaliacaoFisica = avaliacaoFisica;
+    }
+
+    public Academia getAcademia() {
+        return academia;
+    }
+
+    public void setAcademia(Academia academia) {
+        this.academia = academia;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
--- codigofonte/project-main/src/main/java/com/academia/model/Treino.java
+++ codigofonte/project-main/src/main/java/com/academia/model/Treino.java
@@ -1,11 +1,16 @@
+package com.academia.model;
+
 import java.util.Date;
 
-public class Treino{
+public class Treino {
     private int idTreino;
     private String descricao;
     private Date dataInicio;
     private Date dataFim;
     private Professor professor;
+
+    public Treino() {
+    }
 
     public Treino(int idTreino, String descricao, Date dataInicio, Date dataFim, Professor professor) {
         this.idTreino = idTreino;
@@ -13,5 +18,45 @@ public class Treino{
         this.dataInicio = dataInicio;
         this.dataFim = dataFim;
         this.professor = professor;
-        }
+    }
+
+    public int getIdTreino() {
+        return idTreino;
+    }
+
+    public void setIdTreino(int idTreino) {
+        this.idTreino = idTreino;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
 }
